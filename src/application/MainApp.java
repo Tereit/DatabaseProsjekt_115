69b1a67e83ed29a7762eaec467b133be0665a14c
Morten Mulvik <morten_mulvik@hotmail.com>
2017-03-09 18:35:56 +0100

    Added new functionality
    Added a method to show alert window, controllers can use this to show
    errors/warnings to the user.
    Added a RootController object.
    Moved static field for the database connection info to MainApp.java from
    Database.java

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -1,13 +1,20 @@
 package application;
 
+import application.view.RootController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	public static final String URL = "jdbc:mysql://localhost/treningsdagbok";
+
+	//static final String USER = "user";
+	//static final String PASS = "password";
 	
 	@Override
 	public void start(Stage stage) {
@@ -15,6 +22,9 @@ public class MainApp extends Application {
 			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("view/RootPane.fxml"));
 			AnchorPane root = loader.load();
+			
+			RootController controller = loader.getController();
+			controller.setMainApp(this);
 			
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
@@ -29,5 +39,13 @@ public class MainApp extends Application {
 	
 	public static void main(String[] args) {
 		launch(MainApp.class, args);
+	}
+	
+	public void postAlert(AlertType type, String title, String content) {
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setContentText(content);
+		
+		alert.showAndWait();
 	}
 }
